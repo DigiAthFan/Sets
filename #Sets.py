f599d4211bb194afd2fbf74dd2d5c7b09c7ddbc2
A_Hasan_C <legendofotherworlds@gmail.com>
2025-05-22 11:02:55 +0530

    Add files via upload

---
+++ #Sets.py
@@ -0,0 +1,7 @@
+#Sets
+E = {0, 2, 4, 6, 8}  
+N = {1, 2, 3, 4, 5, 6}
+print("Union of E and N is",  E | N) 
+print("Interseciton of E and N is", E & N)
+print("difference of E and N is", E - N)
+print ("Symmetric difference of E and N is", E ^ N)
